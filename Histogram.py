5e0ce4f43da874428a050c74ad114f19ea7326c2
alach2 <alach@dons.usfca.edu>
2024-04-30 21:19:56 -0700

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -41,15 +41,59 @@ def republican_wins(partition):
             republican_won += 1
     return republican_won
 
+# Define a function to determine the mean median difference
+def mean_median_difference(partition):
+    republican_voting_population = sum(partition["rep votes"].values())
+    democratic_voting_population = sum(partition["dem votes"].values())
+
+    rep_median = 0
+    dem_median = 0
+
+    total_population = sum(partition["population"].values())
+
+    for district in partition.parts:
+        rep_median += partition["rep votes"][district] / republican_voting_population * partition["population"][district]
+        dem_median += partition["dem votes"][district] / democratic_voting_population * partition["population"][district]
+
+    mean_median_diff = (rep_median - dem_median) / total_population
+
+    return mean_median_diff
+
+# Define a function to determine the efficiency gap
+def efficiency_gap(partition):
+    republican_wasted_votes = 0
+    democratic_wasted_votes = 0
+
+    for district in partition.parts:
+        democratic_votes = partition["dem votes"][district]
+        republican_votes = partition["rep votes"][district]
+        total_votes = democratic_votes + republican_votes
+
+        if democratic_votes > republican_votes:
+            democratic_wasted_votes += (democratic_votes - (total_votes / 2))
+            republican_wasted_votes += republican_votes
+        else:
+            republican_wasted_votes += (republican_votes - (total_votes / 2))
+            democratic_wasted_votes += democratic_votes
+
+    total_votes = sum(partition["population"].values())
+
+    efficiency_gap_value = (republican_wasted_votes - democratic_wasted_votes) / total_votes
+
+    return efficiency_gap_value
+
+
 initial_partition = Partition(
       graph, 
       assignment="CD",
       updaters={
-          "cut_edges":cut_edges, 
+          "cut_edges": cut_edges, 
           "population": Tally("ALL_TOT20", alias="population"),  
           "rep votes": Tally("PRES20R", alias = "rep votes"),
           "dem votes": Tally("PRES20D", alias = "dem votes"),
           "republican_won": republican_wins,
+          "efficiency_gap": efficiency_gap,
+          "mean_median_difference": mean_median_difference,
       }
 )
 
@@ -79,6 +123,8 @@ our_random_walk = MarkovChain(
 # Lists to store data for histograms
 cutedge_ensemble = []
 republican_won_ensemble = []
+efficiency_gap_ensemble = []
+mean_median_difference_ensemble = []
 
 # Running the Markov Chain
 for part in our_random_walk:
@@ -86,6 +132,10 @@ for part in our_random_walk:
     cutedge_ensemble.append(len(part["cut_edges"]))
     
     republican_won_ensemble.append(part["republican_won"])
+    
+    efficiency_gap_ensemble.append(part["efficiency_gap"])
+    
+    mean_median_difference_ensemble.append(part["mean_median_difference"])
 
 plt.figure()
 plt.hist(cutedge_ensemble, align = 'left')
@@ -97,4 +147,16 @@ plt.figure()
 plt.hist(republican_won_ensemble, align = 'left')
 plt.title("Histogram of Republican-won districts")
 plt.show()
-#plt.savefig('histogram_democratic_won.png')
+#plt.savefig('histogram_republican_won.png')
+
+plt.figure()
+plt.hist(efficiency_gap_ensemble, align = 'left')
+plt.title("Histogram of Efficiency Gap")
+plt.show()
+#plt.savefig('histogram_efficiency_gap.png')
+
+plt.figure()
+plt.hist(mean_median_difference_ensemble, align = 'left')
+plt.title("Histogram of Mean Median Difference")
+plt.show()
+#plt.savefig('histogram_mean_median.png')
